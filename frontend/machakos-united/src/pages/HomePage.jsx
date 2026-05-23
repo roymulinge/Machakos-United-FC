425494ec73828598619d135e403bf25871932ed4
Roy <mutuaroymulinge@gmail.com>
2026-05-23 13:34:58 +0300

    feat(homepage): initialize page create a slighly gray background color easy for eyes , create a hero container with background as the stadium image,add a dark overlay for readability

---
+++ frontend/machakos-united/src/pages/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from "React";
+
+const HomePage = () => {
+    return (
+        <div className="bg-gray-50 font-sans">
+          <header className="relative h-screen bg-cover bg-center bg-no-repeat"
+                  style={{backgroundImage: "url('/images/hero-stadium.jpg)"}}>
+                <div className="absolute inset-0 bg-black/50">
+                    
+                </div>
+
+          </header>
+
+        </div>
+    );
+
+};
+
+export default HomePage;
