412e42e0496e3e688e59da75973ac5a04f22e82b
Roy <mutuaroymulinge@gmail.com>
2026-05-23 13:43:03 +0300

    Add: make the content wrapper text at the center of the page and make the text white

--- frontend/machakos-united/src/pages/HomePage.jsx
+++ frontend/machakos-united/src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ const HomePage = () => {
           <header className="relative h-screen bg-cover bg-center bg-no-repeat"
                   style={{backgroundImage: "url('/images/hero-stadium.jpg)"}}>
                 <div className="absolute inset-0 bg-black/50">
-                    <div className="relaive z-10 flex">
+                    <div className="relaive z-10 flex flex-col items-center justify-center h-full text-center text-white px-4">
                         <h1>Machakos United FC</h1>
                         <p>Pride of Machakos </p>
                         <button>Buy Tickets</button>
