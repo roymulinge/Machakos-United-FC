04545db2b0440a4bb1077a9d326befcb137b8fc4
Roy <mutuaroymulinge@gmail.com>
2026-05-23 13:38:05 +0300

    Add a div class to wrap the content

--- frontend/machakos-united/src/pages/HomePage.jsx
+++ frontend/machakos-united/src/pages/HomePage.jsx
@@ -6,7 +6,11 @@ const HomePage = () => {
           <header className="relative h-screen bg-cover bg-center bg-no-repeat"
                   style={{backgroundImage: "url('/images/hero-stadium.jpg)"}}>
                 <div className="absolute inset-0 bg-black/50">
-                    
+                    <div>
+                        <h1>Machakos United FC</h1>
+                        <p>Pride of Machakos </p>
+                        <button>Buy Tickets</button>
+                    </div>
                 </div>
 
           </header>
