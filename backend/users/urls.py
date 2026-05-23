dd746b5527504cb06e08d77be6cdfde9fbb37bfc
Roy <mutuaroymulinge@gmail.com>
2026-05-23 18:42:33 +0300

    fix(users-urls): remove beginning / and api from the url patterns

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'users'
 
 urlpatterns =[
-    path('api/register/', views.RegisterView.as_view(), name='register'),
-    path('api/login/', views.LoginView.as_view(), name='login'),
-    path('api/profile/', views.ProfileView.as_view(), name='profile'),
-    path('api/password-reset/', views.PasswordResetRequestView.as_view(), name='password_reset'),
-    path('api/password-reset/confirm/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('password-reset/', views.PasswordResetRequestView.as_view(), name='password_reset'),
+    path('password-reset/confirm/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
